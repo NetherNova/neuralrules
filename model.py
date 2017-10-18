07f260b7ed3b7c7ac13016cc2726f22faedd96ae
NetherNova <martin.ringsquandl@googlemail.com>
2017-10-18 11:55:49 +0200

    batch outside loop

--- model.py
+++ model.py
@@ -12,22 +12,25 @@ class SimpleQuery(object):
     def create_graph(self):
 
         def body(x, previous):
-            # TODO: consider batch!
+            # TODO: consider batch! a_matrix : 2, 5, 3 ==> a_vector : 5, 3
             a_vector = tf.nn.embedding_lookup(self.a_matrix, x)
 
-            # [1, k] [k * E, E]
-            u_sum = tf.reduce_sum(tf.reshape(a_vector, [self.num_relations, 1, 1]) * self.operator_tensor, 0)
-            # [3,1,1] * [3, 4, 4]
+            # u_sum = tf.reduce_sum(tf.reshape(a_vector, [self.num_relations, 1, 1]) * tf.expand_dims(self.operator_tensor, 0), 0)
+            # reduce either axis 2, or 3
+            u_sum = tf.reduce_sum(tf.multiply(tf.reshape(a_vector, [-1, self.num_relations, 1, 1]), tf.expand_dims(self.operator_tensor, 0)), 3)
+            # [5, 3, 1, 1] * [1, 3, 4, 4] => [5, 3, 4]
 
-            return [x+1, tf.reduce_sum(u_sum * previous, 1)] # is input for next round
+            # [5,3,4] * [5,4]
+            return [x+1, tf.matmul(u_sum, tf.expand_dims(previous, 1))]
+            # return [x+1, tf.reduce_sum(u_sum * previous, 1)] # is input for next round
 
         def condition(x, previous):
             return tf.greater(self.max_rule_len, x)
 
         # placeholders for batch training
-        self.x = tf.placeholder(tf.int32, [1])
-        self.y = tf.placeholder(tf.int32, [1])
-        self.query = tf.placeholder(tf.int32, [1])
+        self.x = tf.placeholder(tf.int32, [None])
+        self.y = tf.placeholder(tf.int32, [None])
+        self.query = tf.placeholder(tf.int32, [None])
         self.ops = tf.constant(self.operator_tensor, dtype=tf.float32)
 
         self.x_oh = tf.one_hot(self.x, depth=self.num_entities, dtype=tf.float32)
@@ -39,25 +42,26 @@ class SimpleQuery(object):
         self.b = tf.Variable(tf.random_normal([self.num_relations, self.max_rule_len, self.num_relations]))
 
         # a matrix for each query in batch
-        self.a_matrix = tf.squeeze(tf.nn.embedding_lookup(self.a, self.query), 0) # for these queries
+        # self.a_matrix = tf.squeeze(tf.nn.embedding_lookup(self.a, self.query), 0) # for these queries
+        # switch batch to second axis, to be able to select from *time* on first axis
+        self.a_matrix = tf.transpose(tf.nn.embedding_lookup(self.a, self.query), [1, 0, 2])
 
         self.operator_tensor_reshape = tf.reshape(self.operator_tensor, [-1, self.num_entities])
+        self.a_vector = tf.nn.embedding_lookup(self.a_matrix, 0)
 
-        a_vector = tf.nn.embedding_lookup(self.a_matrix, 0)
-        self.u = tf.reduce_sum(tf.multiply(self.operator_tensor_reshape, self.x_oh), axis=1)
+        # via matmul it is already summed
+        self.u = tf.matmul(self.operator_tensor_reshape, tf.transpose(self.x_oh))
+        self.u_reshape = tf.transpose(tf.reshape(self.u, [num_entities, num_relations, -1]), [2, 0, 1])
 
-        self.u_reshape = tf.multiply(tf.reshape(a_vector, [self.num_relations, 1]), tf.reshape(self.u, [self.num_relations, self.num_entities]))
+        self.u_sum = tf.squeeze(tf.matmul(self.u_reshape, tf.expand_dims(self.a_vector, 2)), 2)
+        # self.u = tf.reduce_sum(tf.multiply(tf.transpose(self.operator_tensor_reshape), self.x_oh), axis=1)
+        # self.u_reshape = tf.multiply(tf.reshape(self.a_vector, [self.num_relations, 1]),
+        #                              tf.reshape(self.u, [self.num_relations, self.num_entities]))
 
         # sum up outcomes of all weighted relation operators
-        self.u_sum = tf.reduce_sum(self.u_reshape, axis=0)
-
-        # # TODO: within loop (batch query lookup selection inside the loop)
-        #
+        # self.u_sum = tf.reduce_sum(self.u_reshape, axis=0)
         # self.a_vector = tf.nn.embedding_lookup(self.a_matrix, 0) # first time step for this query
-        #
-        # # operator_tensor to matrix
 
-        #
         # # first operator multiply with input x over all relation operators
         # self.u = tf.reduce_sum(tf.multiply(self.operator_tensor_reshape, self.x_oh), axis=1)
         # # reshape back to all relation matrices and weight with respective a
@@ -66,12 +70,12 @@ class SimpleQuery(object):
         # self.u_sum = tf.reduce_sum(u_reshape, axis=0)
 
         # can pass multiple arguments loop_vars
-        self.result = tf.while_loop(condition, body, [1, self.u_sum])[1]
+        #self.result = tf.while_loop(condition, body, [1, self.u_sum])[1]
 
         # TODO: end loop
 
-        self.loss = -tf.reduce_sum(tf.multiply(self.y_oh, self.result))
-        self.optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(self.loss)
+        #self.loss = -tf.reduce_sum(tf.multiply(self.y_oh, self.result))
+        #self.optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(self.loss)
 
     def loss(self):
         return [self.optimizer, self.loss]
@@ -97,13 +101,28 @@ if __name__ == '__main__':
     #operator_tensor[2][0][2] = 1
 
     sq = SimpleQuery(max_rule_len, operator_tensor, num_entities, num_relations)
+    x_batch = [0, 0, 0, 0, 0]
+    y_batch = [2, 2, 2, 2, 2]
+    query_batch = [0, 0, 0, 0, 0]
+
     with tf.Session() as sess:
         sq.create_graph()
         tf.global_variables_initializer().run()
         coeffs_tmp = None
-        for x in range(10):
-            _, loss, op_reshape = sess.run([sq.optimizer, sq.loss, sq.u_sum], feed_dict={sq.x : [0], sq.y : [2], sq.query: [0]})
-            print "Loss", loss, op_reshape
+        for x in range(2):
+            x_, y_, a_matrix, query, a_vector, u, u_reshape, u_sum = sess.run([sq.x_oh, sq.y_oh, sq.a_matrix, sq.query,
+                                                             sq.a_vector, sq.u, sq.u_reshape, sq.u_sum],
+                                               feed_dict= { sq.x : x_batch, sq.y : y_batch, sq.query: query_batch} )
+            print("x:", x_.shape)
+            print("y:", y_.shape)
+            print("a_matrix:", a_matrix.shape)
+            print("query", query.shape)
+            print("a_vector", a_vector.shape)
+            print("u", u.shape)
+            print("u_reshape", u_reshape.shape)
+            print("u_sum", u_sum.shape)
+            #_, loss, op_reshape = sess.run([sq.optimizer, sq.loss, sq.u_sum], feed_dict={sq.x : [0], sq.y : [2], sq.query: [0]})
+            #print "Loss", loss, op_reshape.shape
 
             coeffs = sess.run(sq.a)
             if coeffs_tmp is None:
